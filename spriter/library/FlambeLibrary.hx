32db1d15b5b1bf09ee644c65e0267f72a1ec74b3
Quinn Hoener <quinn@seven2.com>
2014-10-09 09:36:49 -0700

    Updated to set sprite positions using their matrix.
    Signed-off-by: Quinn Hoener <quinn@seven2.com>

--- spriter/library/FlambeLibrary.hx
+++ spriter/library/FlambeLibrary.hx
@@ -5,6 +5,7 @@ import flambe.display.ImageSprite;
 import flambe.display.Sprite;
 import flambe.display.Texture;
 import flambe.Entity;
+import flambe.math.FMath;
 import flambe.math.Point;
 import flambe.math.Matrix;
 import spriter.definitions.PivotInfo;
@@ -43,11 +44,17 @@ class FlambeLibrary extends AbstractLibrary
 	}
 	override public function getFile(name:String):Dynamic
 	{
+		return null;
+	}
+
+	private function getTexture(name:String):Texture
+	{
 		var aSplitName:Array<String> = name.split(".");
 		aSplitName.pop();
 		name = aSplitName.join("");
 		return _pack.getTexture(_basePath + "/" + name);
 	}
+
 	override public function clear():Void
 	{
         _rootEntity.disposeChildren();
@@ -55,16 +62,20 @@ class FlambeLibrary extends AbstractLibrary
     }
 	override public function addGraphic(group:String, timeline:Int, key:Int, name:String, info:SpatialInfo, pivots:PivotInfo):Void
 	{
-		var bmp:Texture = cast getFile(name);
+		var bmp:Texture = getTexture(name);
 
 		var spatialResult:SpatialInfo = compute(info, pivots, bmp.width, bmp.height);
 		
 		// Setup the Flambe Sprite.		
 		var sprite:ImageSprite = new ImageSprite(bmp);
-		sprite.setXY(spatialResult.x, spatialResult.y);
-		sprite.setRotation(SpriterUtil.fixRotation(spatialResult.angle));
-		sprite.setScaleXY(spatialResult.scaleX, spatialResult.scaleY);
-		sprite.setAlpha(spatialResult.a);
+		var localMatrix:Matrix = sprite.getLocalMatrix();
+		localMatrix.compose(spatialResult.x, spatialResult.y, spatialResult.scaleX, spatialResult.scaleY, FMath.toRadians(SpriterUtil.fixRotation(spatialResult.angle)));
+        // localMatrix.translate(-anchorX._, -anchorY._);
+
+		// sprite.setXY(spatialResult.x, spatialResult.y);
+		// sprite.setRotation(SpriterUtil.fixRotation(spatialResult.angle));
+		// sprite.setScaleXY(spatialResult.scaleX, spatialResult.scaleY);
+		sprite.alpha._ = spatialResult.a;
 		
 		// Add as new child to the root. 
 		_rootEntity.addChild(new Entity().add(sprite));
